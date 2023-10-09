65da1fad659ec043179a9498b9b4025041ed864f
Abdullah Abdelhafez <beedoz377@gmail.com>
2023-10-09 21:20:05 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+def CalculateCooccurence():
+    pass
+
+def CalculateContrast():
+    pass
+
+def CalculateHistogram():
+    pass
+
+def CalculateCumulativeHistogram():
+    pass
+
+def GetColorAtPercentage():
+    pass
+
+def StretchContrast():
+    pass
+
+def EqualizeHistogram():
+    pass
+
+def GrayScaleTransformation():
+    pass
+
