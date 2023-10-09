6efbd7505ee017d1220c65a3e3055e688db60ea6
Abdullah Abdelhafez <beedoz377@gmail.com>
2023-10-09 21:40:05 +0300

    Converted images to arrays

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from matplotlib.pyplot import imread
+image1 = imread("img/image1.png")
+image2 = imread("img/image2.png")
+image3 = imread("img/image3.png")
+
 def CalculateCooccurence():
     pass
 
