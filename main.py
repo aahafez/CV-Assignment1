e33772dc12f9fb4b187fcae81943cb69194b9519
Abdullah Abdelhafez <beedoz377@gmail.com>
2023-10-13 16:20:20 +0300

    Added CalculateCooccurence function

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from matplotlib.pyplot import imread
-image1 = imread("img/image1.png") # Converts image to 2D Array
-image2 = imread("img/image2.png")
-image3 = imread("img/image3.png")
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+def ReadImage(name):
+    print('Reading image:', name)
+    img = Image.open('img/' + name + '.png')
+    return img
 
-def CalculateCooccurence():
-    pass
+def PrintImageInfo(image):
+    height, width = image.shape
+    print(image)
+    print("Minimum:", np.min(image))
+    print("Maximum:", np.max(image))
+    print("Height: ", height, "px", sep="")
+    print("Width: ", width, "px", sep="")
+    print("----------------------------------------")
+def CalculateCooccurence(image):
+    imgArr = np.array(image)
+    height, width = imgArr.shape
+    matrix = [[0]*256]*256
+    for i in range(height-1):
+        for j in range(width):
+            index1 = imgArr[i][j]
+            index2 = imgArr[i+1][j]
+            matrix[index1][index2] += 1
+            #     image.putpixel((j,i),0)
+    return matrix
+
+image1 = ReadImage('image1')
+PrintImageInfo(np.array(image1))
+cooccurence = CalculateCooccurence(image1)
+print(cooccurence)
 
 def CalculateContrast():
     pass
